056464ed679595e669e85ed8fac3c07c73e60f5a
Vitaly Karpenko <vitaly-karpenko@outlook.com>
2019-11-03 13:41:53 +0300

    calc works

--- main.py
+++ main.py
@@ -7,28 +7,44 @@ from config import (
     FIDESYS_VERSION,
 )
 import os
+import sys
 
-os.environ["PYTHONPATH"] = (
-    ";".join(FIDESYS_BASE_FOLDER + subpath for subpath in FIDESYS_PYTHONPATH)
+for subpath in FIDESYS_PYTHONPATH:
+    sys.path.append(FIDESYS_BASE_FOLDER + subpath)
+
+os.environ["PATH"] = (
+    ";".join(FIDESYS_BASE_FOLDER + subpath for subpath in FIDESYS_LIBPATH)
     + ";"
-    + os.environ.get("PYTHONPATH", "")
+    + os.environ.get("PATH", "")
 )
-
-print(os.environ["PYTHONPATH"])
 
 import subprocess
 from fidesys import FidesysComponent
 import cubit
 
-displacement_x = 0
-displacement_y = 0
+displacement_x = 0.1
+displacement_y = 0.1
 
 with open("template_2d.txt") as template:
-    script = template.read().format(
-        displacement_x=displacement_x, displacement_y=displacement_y
+    script = (
+        template.read()
+        .format(displacement_x=displacement_x, displacement_y=displacement_y)
+        .split("\n")
     )
 
 
 cubit.init([""])
 fc = FidesysComponent()
 fc.start_up_no_args()
+
+for command in script:
+    cubit.cmd(command)
+fc.writeFC(r"C:\Projects\effective_yield_surface\test.fc", True)
+subprocess.call(
+    [
+        FIDESYS_CALC,
+        r"--input=C:\Projects\effective_yield_surface\test.fc",
+        r"--output=C:\Projects\effective_yield_surface\test.pvd",
+    ]
+)
+# cubit.destroy()
