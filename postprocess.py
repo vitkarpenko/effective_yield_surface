e3560376c5c1a8c864e82729df03dae12834dfb3
Vitaly Karpenko <vitkarpenko@gmail.com>
2019-11-23 14:24:44 +0300

    master max mises

--- postprocess.py
+++ postprocess.py
@@ -6,6 +6,4 @@ data = servermanager.Fetch(reader)
 point_data = data.GetPointData()
 array = point_data.GetArray("Stress")
 number_of_components = array.GetNumberOfComponents()
-for i in range(number_of_components):
-    print(i, array.GetComponentName(i))
-print("max", max(array.GetComponent(i, 6) for i in range(array.GetNumberOfTuples())))
+max_mises = max(array.GetComponent(i, 6) for i in range(array.GetNumberOfTuples()))
