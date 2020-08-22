223591737bf56c0bf16ed824a33fd3db275a8b46
Vitaly Karpenko <vitaly-karpenko@outlook.com>
2020-08-22 15:57:07 +0300

    plots

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import subprocess
 from math import cos, radians, sin
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from config import PVPYTHON
 from utils import cleanup
+
+sns.set(style="ticks")
+sns.palplot(sns.cubehelix_palette(8, start=0.5, rot=-0.75))
 
 cleanup(start=True)
 
@@ -19,5 +25,17 @@ for phi in range(0, 361, 20):
         ]
     )
     subprocess.call([PVPYTHON, r"C:\Projects\effective_yield_surface\postprocess.py"])
+
+dots_x = []
+dots_y = []
+with open("outs.csv", "r") as outs:
+    for line in outs:
+        splitted = line.strip().split(",")
+        x, y = float(splitted[0]), float(splitted[1])
+        dots_x.append(x)
+        dots_y.append(y)
+
+plt.plot(dots_x, dots_y, "bo")
+plt.show()
 
 cleanup()
